dfef68ea1aa36002f0ae454262eb681c7eed8264
Shawn Wood <Shawnwood97@gmail.com>
2021-04-27 18:15:47 -0600

    basic registration form completed, axios and cookies packages installed, logic next

--- src/components/RegisterUser/UserRegistration.vue
+++ src/components/RegisterUser/UserRegistration.vue
@@ -1,5 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <form action="javascript:void(0)">
+      <label for="email">Email Address</label>
+      <input type="email" name="email" id="emailInput" />
+      <label for="username">Username</label>
+      <input type="text" name="username" id="usernameInput" />
+      <label for="password">Password</label>
+      <input type="password" name="password" id="passwordInput" />
+      <label for="confirmPassword">Confirm Password</label>
+      <input type="password" name="confirmPassword" id="confirmPasswordInput" />
+      <label for="headline">Headline</label>
+      <input type="text" name="headline" id="headline" />
+      <label for="birthDate">Date Of Birth</label>
+      <input
+        type="date"
+        name="birthDate"
+        id="birthDateInput"
+        value="2020-04-20"
+      />
+    </form>
+  </div>
 </template>
 
 <script>
